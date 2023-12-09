03964bcb8fc046e3f84d41c1366649f07bbd5f0b
TESLANET <gokhanozdemir_77@hotmail.com>
2023-12-09 17:34:17 +0300

    services

---
+++ test/XUnitCRUDTest/.vshistory/ProductServiceTest.cs/2023-12-09_16_40_40_447.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture;
+using ECommerce.Core.DTOs.Request;
+using ECommerce.Core.ServiceContracts;
+using ECommerce.Core.Services;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace XUnitCRUDTest
+{
+    public class ProductServiceTest
+    {
+        IProductService _productService;
+        ITestOutputHelper _testOutputHelper;
+        Fixture _fixture;
+
+      
+
+        public ProductServiceTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+            _productService = new ProductService();
+            _fixture = new Fixture();
+        }
+
+        [Fact]
+        public void GetProduct_CheckArgumentNull_ThrowArgumentNullExcepiton()
+        {
+
+            //Arrange:
+            GetProductRequest request = null;
+
+            //Act:
+            _testOutputHelper.WriteLine("this is test");
+            //_testOutputHelper.WriteLine(request.ToString());
+
+            //Assert:
+            Assert.Throws<ArgumentNullException>(
+                () => {
+                    //Act:
+                    _productService.GetProduct(request);
+                    }
+                );
+
+        }
+
+        [Fact]
+        public void GetProduct_GetSuccedResponse_WithProperRequest()
+        {
+
+            //Arrange:
+            GetProductRequest request = _fixture.Create<GetProductRequest>();
+
+
+            //Act:
+            _testOutputHelper.WriteLine("this is test");
+            var response = _productService.GetProduct(request);
+
+            //Assert:
+            Assert.True(response.Id > 0);
+
+        }
+
+    }
+}
