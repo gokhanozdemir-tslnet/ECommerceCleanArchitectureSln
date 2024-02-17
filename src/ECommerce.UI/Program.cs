b8c9217cae88530dcab8678cbbdd72b54537b36c
TESLANET <gokhanozdemir_77@hotmail.com>
2024-02-17 15:18:21 +0300

    adding area

--- src/ECommerce.UI/Program.cs
+++ src/ECommerce.UI/Program.cs
@@ -72,6 +72,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "Admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
